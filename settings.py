e52ce4db05a37bc13d7644c5d5306b5515975898
SomeoneNamedDwarf <derpypierus@gmail.com>
2025-10-07 21:59:06 +0700

    moved app specific css to a separate static directory

--- settings.py
+++ settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / "staticfiles" ]
 
 # Default primary key field type
